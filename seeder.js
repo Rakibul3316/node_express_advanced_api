dfdbf2cd0a92e8d3b7ea7fab786d0944bf42fc8a
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2022-08-15 01:21:10 +0600

    create course model and seed course data into db.

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -15,12 +16,19 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 // Read JSON Files
-const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+const bootcamps = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // Import in DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
 
         console.log("Data Imported...".green.inverse);
         process.exit();
@@ -33,6 +41,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
 
         console.log("Data Destroyed...".red.inverse);
         process.exit();
